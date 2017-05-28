09923f8b8318858b67147890578eeea860dbd3af
pm0355 <pm0355a@student.american.edu>
2017-05-28 14:41:29 -0400

    apache cookbook configuration

---
+++ apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'apache'
+maintainer       'Paul Mattioli'
+maintainer_email 'mattioli.paul@gmail.com'
+license          'MIT'
+description      'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version          '0.1.0'
